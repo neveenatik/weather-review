d544cf98c4bc1a956d2bdcc764c19b78da92f87f
zmagzoub <zaid.majzoub1@gmail.com>
2018-10-17 00:19:02 +0200

    added belgium

---
+++ Belguim/output.js
@@ -0,0 +1,199 @@
+[
+  {
+    "location": {
+      "name": "Bierset",
+      "lat": 50.65513,
+      "lng": 5.45131
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 16,
+        "humidityPercent": 60,
+        "pressureHPA": 1019,
+        "windDirectionDeg": 157.5,
+        "windSpeedMps": 3.89,
+        "symbol": "few clouds"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Buzenol",
+      "lat": 49.64731,
+      "lng": 5.59564
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 8,
+        "humidityPercent": 94,
+        "pressureHPA": 1021,
+        "windDirectionDeg": 22.5,
+        "windSpeedMps": 2.78,
+        "symbol": "-"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Chièvres",
+      "lat": 50.58801,
+      "lng": 3.80654
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 13,
+        "humidityPercent": 78,
+        "pressureHPA": 1018,
+        "windSpeedMps": 1.11,
+        "symbol": "-"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Deurne",
+      "lat": 50.65513,
+      "lng": 5.45131
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 13,
+        "humidityPercent": 78,
+        "pressureHPA": 1018,
+        "windDirectionDeg": 180,
+        "windSpeedMps": 1.11,
+        "symbol": "fair"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Diepenbeek",
+      "lat": 50.90778,
+      "lng": 5.41969
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 10,
+        "humidityPercent": 99,
+        "pressureHPA": 1019,
+        "windDirectionDeg": 67.5,
+        "windSpeedMps": 1.11,
+        "symbol": "-"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Florennes",
+      "lat": 50.25101,
+      "lng": 4.60361
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 13,
+        "humidityPercent": 77,
+        "pressureHPA": 1019,
+        "windSpeedMps": 1.11,
+        "symbol": "-"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Kleine-Brogel",
+      "lat": 51.17194,
+      "lng": 5.45038
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 15,
+        "humidityPercent": 67,
+        "pressureHPA": 1019,
+        "windSpeedMps": 1.11,
+        "symbol": "-"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Middelkerke",
+      "lat": 51.18514,
+      "lng": 2.82085
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 14,
+        "humidityPercent": 88,
+        "pressureHPA": 1018,
+        "windDirectionDeg": 270,
+        "windSpeedMps": 3.06,
+        "symbol": "very cloudy"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Saint-Hubert",
+      "lat": 50.02606,
+      "lng": 5.3741
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 13,
+        "humidityPercent": 67,
+        "pressureHPA": 1020,
+        "windDirectionDeg": 135,
+        "windSpeedMps": 3.06,
+        "symbol": "fair"
+      }
+    ]
+  },
+  {
+    "location": {
+      "name": "Uccle",
+      "lat": 50.81146,
+      "lng": 4.3536
+    },
+    "weather": [
+      {
+        "updatedTimestamp": 1539727200,
+        "fromHour": 1539727200,
+        "toHour": 1539730800,
+        "temperatureC": 14,
+        "humidityPercent": 76,
+        "pressureHPA": 1017,
+        "windDirectionDeg": 180,
+        "windSpeedMps": 3.33,
+        "symbol": "few clouds"
+      }
+    ]
+  }
+]
